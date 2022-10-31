0a22e583eba0e8d9194f37295647b98994b96f91
BaurinAnton <baurinanton2013@yandex.ru>
2022-10-31 14:08:41 +0300

    config database local bases

--- config/database.js
+++ config/database.js
@@ -5,15 +5,15 @@ module.exports = ({ env }) => ({
       connector: 'mongoose',
       settings: {
         host: env('DATABASE_HOST'),
-        srv: env.bool('DATABASE_SRV', true),
-        port: env.int('DATABASE_PORT', 27017),
+        srv: env.bool('DATABASE_SRV', false),
+        port: env.int('DATABASE_PORT'),
         database: env('DATABASE_NAME'),
         username: env('DATABASE_USERNAME'),
         password: env('DATABASE_PASSWORD')
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', true)
+        ssl: env.bool('DATABASE_SSL', false)
       }
     }
   }
